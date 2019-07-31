ffa1cfd6326a49592e2fec7aebfa3d26c3d1b281
Martin Ohmann <martin.ohmann@bonial.com>
2019-07-31 16:44:49 +0200

    change logic to use constant backoff

--- spinnaker/api/retry.go
+++ spinnaker/api/retry.go
@@ -3,34 +3,39 @@ package api
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/cenkalti/backoff"
 )
 
-// maxRetries defines the number of times an operation should be retried in the
-// worst case.
-const maxRetries = 5
+const (
+	// maxRetries defines the number of times an operation should be retried in
+	// the worst case.
+	maxRetries = 5
 
-// operation is a func that performs an API call.
+	// retryInterval specified the time to wait between two retries.
+	retryInterval = 2 * time.Second
+)
+
+// operation is a func that performs an API call and returns the response json
+// data as map, the http response and an error.
 type operation func() (map[string]interface{}, *http.Response, error)
 
-// retry retries an operation using constant backoff. Since non-2xx status
-// codes do not cause errors, we handle them separately to force retries for
-// 5xx responses.
-func retry(fn operation) (payload map[string]interface{}, resp *http.Response, err error) {
+// retry retries an operation using constant backoff.
+func retry(fn operation) (data map[string]interface{}, resp *http.Response, err error) {
 	err = backoff.Retry(func() error {
-		payload, resp, err = fn()
+		data, resp, err = fn()
 
 		// Check the response code. We retry on 500-range responses to allow
 		// the server time to recover, as 500's are typically not permanent
 		// errors and may relate to outages on the server side. This will catch
 		// invalid response codes as well, like 0 and 999.
-		if resp != nil && (resp.StatusCode == 0 || resp.StatusCode >= http.StatusInternalServerError) {
+		if resp != nil && (resp.StatusCode == 0 || resp.StatusCode >= 500) {
 			return errors.New(http.StatusText(resp.StatusCode))
 		}
 
 		return err
-	}, backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries))
+	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(retryInterval), maxRetries))
 
 	return
 }
